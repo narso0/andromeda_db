d45bf02e6a90cd56a1d4f10227c3834ab3655afb
narsia <narsia@>
2026-06-12 10:39:37 +0200

    some trivial cleanup

--- debug_sheets.py
+++ debug_sheets.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
+
+#this file is just for testing the content of the excel file, I just wanted to test if the column names are on the first row.
 file_name = 'Andromeda_Data.xlsx'
-# header=None tells Pandas: "Don't look for headers, just show me the raw data"
-df = pd.read_excel(file_name, sheet_name='Laboratory', header=None)
-print("--- Raw preview of the first 5 rows ---")
+df = pd.read_excel(file_name, sheet_name='Laboratory')
+print("Preview of the first 5 rows:")
 print(df.head(5))
