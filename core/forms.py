b9a304dc795c81679df26955dab4e68fdff61fce
narsia <narsia@>
2026-06-22 15:07:54 +0200

    added scrollbars, serach bars, made the design more appealing, corrected the style of forms

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from .models import Sample, User
 
-class SampleForm(forms.ModelForm):
+
+
+class BootstrapFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs['class'] = 'form-check-input'
+            elif isinstance(field.widget, forms.Select):
+                field.widget.attrs['class'] = 'form-select'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+class SampleForm(BootstrapFormMixin, forms.ModelForm):
     class Meta:
         model = Sample
         fields = '__all__'
@@ -9,10 +21,11 @@ class SampleForm(forms.ModelForm):
         widgets = {
             'reception_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'preparation_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'rows': 3}),
         }
 
 
-class UserForm(forms.ModelForm):
+class UserForm(BootstrapFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
